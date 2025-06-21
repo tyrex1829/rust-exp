defa145db6f2f0a574bf5bdacb654e1e902f368e
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-22 00:18:43 +0530

    booleans in functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+// function, Signed and unsigned number
+
 fn main() {
     let ans: u32 = sum(2, 2);
-    println!("{}", ans)
+
+    let ans_bool: bool = is_even(20);
+
+    println!("ans: {}, ansBool: {}", ans, ans_bool)
 }
 
 fn sum(a: u32, b: u32) -> u32 {
     return a + b
+}
+
+// boolean
+
+fn is_even (n: u32) -> bool {
+    return n % 2 == 0;
 }

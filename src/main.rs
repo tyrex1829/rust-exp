3a29f48741e350fa29416771d8b09a5c1a38bf32
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-22 00:56:39 +0530

    strings in rust

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@
 fn main() {
     let ans: u32 = sum(2, 2);
 
+    // Boolean
     let ans_bool: bool = is_even(20);
 
-    println!("ans: {}, ansBool: {}", ans, ans_bool)
+    // String
+    let first_name = String::from("Tyrex");
+
+    println!("ans: {}, ans_bool: {}, first_name: {}", ans, ans_bool, first_name)
 }
 
 fn sum(a: u32, b: u32) -> u32 {

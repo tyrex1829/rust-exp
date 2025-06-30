ee3428cc478ea9da7bce2b5ff2770345852b414b
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-30 09:25:01 +0530

    mut variables

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 // function, Signed and unsigned number
 
+// fn main() {
+//     let ans: u32 = sum(2, 2);
+
+//     // Boolean
+//     let ans_bool: bool = is_even(20);
+
+//     // String
+//     let first_name = String::from("Tyrex");
+
+//     println!("ans: {}, ans_bool: {}, first_name: {}", ans, ans_bool, first_name)
+// }
+
+// fn sum(a: u32, b: u32) -> u32 {
+//     return a + b
+// }
+
+// // boolean
+
+// fn is_even (n: u32) -> bool {
+//     return n % 2 == 0;
+// }
+
+
 fn main() {
-    let ans: u32 = sum(2, 2);
+    let mut x = 5;
+    println!("The value of x is: {x}");
 
-    // Boolean
-    let ans_bool: bool = is_even(20);
-
-    // String
-    let first_name = String::from("Tyrex");
-
-    println!("ans: {}, ans_bool: {}, first_name: {}", ans, ans_bool, first_name)
-}
-
-fn sum(a: u32, b: u32) -> u32 {
-    return a + b
-}
-
-// boolean
-
-fn is_even (n: u32) -> bool {
-    return n % 2 == 0;
+    x = 6;
+    println!("The value of x is: {x}");
 }
